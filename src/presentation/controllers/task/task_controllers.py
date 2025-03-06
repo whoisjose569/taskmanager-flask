443794c37e9bca4ac90b119bb8989dbabdf6dc6c
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-03-06 15:55:51 -0300

    feat: implementing list_task_by_id controller

--- src/presentation/controllers/task/task_controllers.py
+++ src/presentation/controllers/task/task_controllers.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from http import HTTPStatus
 from src.presentation.schemas.task_schema import CreateTaskSchema
 from src.composers.task.create_task_composer import TaskComposer
+from src.composers.task.list_task_composer import ListTaskComposer
 from src.presenters.task_presenter import TaskPresenter
 
 bp = Blueprint("task", __name__, url_prefix="/tasks")
@@ -18,6 +19,21 @@ def create_task():
         service = TaskComposer.create_task_composer()
 
         response = service.handle(validated_data)
+
+        formatted_response = TaskPresenter.format_response(response)
+        return formatted_response, HTTPStatus.CREATED
+    except Exception as exc:
+        raise
+
+
+@bp.route("/", methods=["GET"])
+def list_task_by_title():
+    try:
+        data = request.json
+
+        service = ListTaskComposer.list_task_composer()
+
+        response = service.handle(data)
 
         formatted_response = TaskPresenter.format_response(response)
         return formatted_response, HTTPStatus.CREATED
